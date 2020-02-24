b8e0e31c094bdc39d30d90224539548b9fc10032
Okafor Irene <okaforirenen@gmail.com>
2020-02-24 23:03:58 +0100

    done with footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,62 @@
 import React from "react";
 import classes from "./Footer.module.css";
+import apple from "../../assets/img/apple.svg";
+import gplay from "../../assets/img/google-play-store.svg";
+import instagram from "../../assets/img/instagram.svg";
+import twitter from "../../assets/img/twitter.svg";
+import facebook from "../../assets/img/facebook.svg"
 
 const footer = props => 
 {
-
   
   return (
     <footer>
 
-        <div></div>
+        <div>
+
+            <p className={classes.FooterHead}>Download App</p>
+
+            <a target="_blank" href="/" className={classes.Download}>
+              <img src={apple} alt="appstore icon"/>
+            </a>
+
+            <a target="_blank" href="/" className={classes.Download}>
+              <img className={classes.Adjust} src={gplay} alt="playstore icon"/>
+            </a>
+
+
+        
+        </div>
+
         <div className={classes.FooterText}>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, aliquam numquam, perferendis modi accusamus harum commodi architecto sed autem sunt distinctio officia repellendus laborum deserunt rerum minima vel? Officia, quasi.
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed sdiam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
             </p>
 
             <a href="#">Lorem ipsum dolor sit</a>
             <a href="#">Lorem ipsum dolor sit</a>
             <a href="#">Lorem ipsum dolor sit</a>
         </div>
-        <div></div>
+
+       <div>
+
+            <p className={classes.FooterHead}>Connect with us</p>
+
+            <a target="_blank" href="/" className={classes.Social}>
+              <img src={twitter} alt="twitter icon"/>
+            </a>
+
+            <a target="_blank" href="/" className={classes.Social}>
+              <img src={facebook} alt="facebook icon"/>
+            </a>
+
+            <a target="_blank" href="/" className={classes.Social}>
+              <img src={instagram} alt="instagram icon"/>
+            </a>
+
+
+        
+        </div>
 
     </footer>
      
